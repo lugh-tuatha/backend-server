c3e97ddad309b51a6058cdfc95011364d6421f68
lugh-tuatha <aceonsteam001@gmail.com>
2024-02-02 00:00:25 +0800

    add: default page when servers open

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const colors = require("colors");
-
 require("dotenv").config();
 
 // Internal Dependencies
 const corsOptions = require('../config/development/CorsOptions.js');
 const connectDB = require('./ConnectDB')
+const { SUCCESS } = require('./constants/shared/success')
+const { ERROR } = require('./constants/shared/error')
 
 const app = express();
 
@@ -15,7 +16,14 @@ app.use(express.json());
 
 app.use(cors(corsOptions));
 
+app.set('view engine', 'ejs')
+
 connectDB();
+
+app.get("/", (req, res) => {
+    // res.send("welcome to server")
+    res.render('index')
+})
 
 app.use("/api", require("./routes/router"))
 
